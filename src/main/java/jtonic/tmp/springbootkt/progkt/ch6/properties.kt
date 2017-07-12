980790ac1d8c0c9d03fcd443e15529c0ec016202
jtonic <antonel.pazargic@gmail.com>
2017-07-12 05:57:54 +0300

    Simple example with multiline string and class properties (custome setters and getters).

--- src/main/java/jtonic/tmp/springbootkt/progkt/ch6/properties.kt
+++ src/main/java/jtonic/tmp/springbootkt/progkt/ch6/properties.kt
@@ -1,25 +1,38 @@
 package jtonic.tmp.springbootkt.progkt.ch6
 
+import jtonic.tmp.springbootkt.progkt.ch3.Person
+
 /**
  * Created by Antonel Ernest Pazargic on 12/07/2017.
  * @author Antonel Ernest Pazargic
  */
 
 fun main(args: Array<String>) {
-    val p = Person()
+    val p = Person("  Antonel ")
     p.age = 47
     println("My age is ${p.age}")
+    println("p.firstName = '${p.firstName}'")
 
+    val multilineStr = """
+        this is
+        a multiline
+        string
+    """
+    println(multilineStr)
 }
 
-class Person {
-    private var _age: Int? = null
+class Person constructor(firstName: String) {
+    var age: Int = 1
+        get() = field
+        set(value) {field = value}
+    var firstName: String = ""
+        get() = field
+        set(value) {field = value.trim()}
 
-    var age: Int?
-        get() = _age
-        set(value) {_age = value}
+
 
     init {
         println("This is the primary constructor")
+        this.firstName = firstName
     }
 }
