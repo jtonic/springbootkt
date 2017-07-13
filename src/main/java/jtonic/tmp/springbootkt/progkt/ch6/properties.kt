23fff1f2b252bac1629c833bba30b112fa7d9032
jtonic <antonel.pazargic@gmail.com>
2017-07-13 05:21:07 +0300

    Secondary constructor and private fields (no properties)

--- src/main/java/jtonic/tmp/springbootkt/progkt/ch6/properties.kt
+++ src/main/java/jtonic/tmp/springbootkt/progkt/ch6/properties.kt
@@ -1,7 +1,5 @@
 package jtonic.tmp.springbootkt.progkt.ch6
 
-import jtonic.tmp.springbootkt.progkt.ch3.Person
-
 /**
  * Created by Antonel Ernest Pazargic on 12/07/2017.
  * @author Antonel Ernest Pazargic
@@ -12,6 +10,14 @@ fun main(args: Array<String>) {
     p.age = 47
     println("My age is ${p.age}")
     println("p.firstName = '${p.firstName}'")
+
+    val p2 = Person()
+    println("p2.firstName = ${p2.firstName}")
+    println("p2 = $p2")
+
+    val p3 = Person2("Irina", 27)
+    println("p3.getAge() = ${p3.getAge()}")
+    println("p3.getFirstName() = ${p3.getFirstName()}")
 
     val multilineStr = """
         this is
@@ -24,15 +30,30 @@ fun main(args: Array<String>) {
 class Person constructor(firstName: String) {
     var age: Int = 1
         get() = field
-        set(value) {field = value}
+        set(value) {
+            field = value
+        }
     var firstName: String = ""
         get() = field
-        set(value) {field = value.trim()}
+        set(value) {
+            field = value.trim()
+        }
 
-
+    constructor(): this("Antonel Ernest")
 
     init {
         println("This is the primary constructor")
         this.firstName = firstName
     }
+
+    override fun toString() = "$firstName, $age"
+}
+
+class Person2(firstName: String, age: Int) {
+    private val firstName: String = firstName
+    private val age: Int = age
+
+    fun getFirstName() = this.firstName
+    fun getAge() = this.age
+
 }
