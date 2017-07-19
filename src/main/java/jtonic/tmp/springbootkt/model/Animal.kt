2f79bf60b2e48bbddbcf477e13f4b5f8451d855a
jtonic <antonel.pazargic@gmail.com>
2017-07-19 06:19:36 +0300

    Revert: Some refactor and doing some work for the lombok shit. (cb7c629) - revert only the lombok apt

--- src/main/java/jtonic/tmp/springbootkt/model/Animal.kt
+++ src/main/java/jtonic/tmp/springbootkt/model/Animal.kt
@@ -7,41 +7,6 @@ package jtonic.tmp.springbootkt.model
  */
 open class Animal (var name: String?, var age: Int) {
 
-    override fun equals(o: Any?): Boolean {
-        if (o === this) {
-            return true
-        }
-        if (o !is Animal) {
-            return false
-        }
-        val other = o as Animal?
-        if (!other!!.canEqual(this as Any)) {
-            return false
-        }
-        val `this$name` = this.name
-        val `other$name` = other.name
-        if (if (`this$name` == null) `other$name` != null else `this$name` != `other$name`) {
-            return false
-        }
-        if (this.age != other.age) {
-            return false
-        }
-        return true
-    }
-
-    override fun hashCode(): Int {
-        val PRIME = 59
-        var result = 1
-        val `$name` = this.name
-        result = result * PRIME + (`$name`?.hashCode() ?: 43)
-        result = result * PRIME + this.age
-        return result
-    }
-
-    protected fun canEqual(other: Any): Boolean {
-        return other is Animal
-    }
-
     override fun toString(): String {
         return "Animal (name=$name, age=$age)"
     }
