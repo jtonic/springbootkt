e08c42be56e923a7f274e50e0ccfbb195ae1a42e
jtonic <antonel.pazargic@gmail.com>
2017-08-08 07:57:48 +0300

    Some simple examples with immutable variables and their initialization. And a simple example with a mutable list.

--- src/test/kotlin/jtonic/tmp/springbootkt/kia/ch6/ArrayTest.kt
+++ src/test/kotlin/jtonic/tmp/springbootkt/kia/ch6/ArrayTest.kt
@@ -1,5 +1,7 @@
 package jtonic.tmp.springbootkt.kia.ch6
 
+import io.kotlintest.matchers.haveSize
+import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
 import org.junit.Test
 
@@ -22,6 +24,13 @@ class ArrayTest {
 
         printElements(*charsAsArray)
 
+    }
+
+    @Test
+    fun `mutable array list`() {
+        val languages = arrayListOf("Java")
+        languages += "Scala"
+        languages should haveSize(2)
     }
 
     private fun printElements(vararg elem: String) {
