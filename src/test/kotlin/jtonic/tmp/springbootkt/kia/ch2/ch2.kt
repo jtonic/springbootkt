39958d878d27cf99cee47d686982ba69b70fd014
jtonic <antonel.pazargic@gmail.com>
2017-08-09 08:16:31 +0300

    when with code blocks in case.

--- src/test/kotlin/jtonic/tmp/springbootkt/kia/ch2/ch2.kt
+++ src/test/kotlin/jtonic/tmp/springbootkt/kia/ch2/ch2.kt
@@ -7,23 +7,30 @@ class Ch2Test {
 
     @Test
     fun `test smart case`() {
-        val e: Expr = Sum(Sum(Num(1), Num(4)), Num(2))
-
-        eval(e) shouldBe 7
+        eval(Sum(Sum(Num(1), Num(4)), Num(2))) shouldBe 7
+        eval(Num(10)) shouldBe 10
     }
 
     private fun eval(e: Expr): Int = when (e) {
-        is Num -> e.value
-        is Sum -> eval(e.left) + eval(e.right)
+        is Num -> {
+            print("$e ")
+            e.value
+        }
+        is Sum -> {
+            val left = e.left
+            val right = e.right
+            print("$e ")
+            eval(left) + eval(right)
+        }
         else -> throw IllegalArgumentException("Unrecognized expression type")
     }
 }
 
 interface Expr
 class Num(val value: Int) : Expr {
-    override fun toString() = value.toString()
+    override fun toString() = "Num($value)"
 }
 
 class Sum(val left: Expr, val right: Expr) : Expr {
-    override fun toString() = "sum of $left and $right"
+    override fun toString() = "Sum($left, $right)"
 }
